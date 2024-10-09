180c81a2071a22f845e47660d4fbdb03a9cd8b8a
gkkconan <conan.gkk@gmail.com>
2024-10-10 00:29:43 +0200

    chore: fix product's page carousel

--- assets/js/productscript.js
+++ assets/js/productscript.js
@@ -8,12 +8,14 @@ function plusSlides(n) {
 
 // Funzione per mostrare la slide corrente
 function showSlides(n) {
-    let i;
     let slides = document.getElementsByClassName("slides");
+    
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
+
+    for(let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
+
     slides[slideIndex - 1].style.display = "block";
 }
